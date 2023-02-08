e1c81259a483fcc0b43a3390e7eded1b5d09bb36
VolodymyrDerkach97 <bobikderkac@gmail.com>
2023-02-08 19:06:46 +0200

    task 1-2 complete

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,40 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const createGgalleryItem = galleryItems.reduce((html, nextEl) => html +
+`<div class="gallery__item">
+  <a class="gallery__link" href="${nextEl.original}">
+    <img
+     class="gallery__image"
+    src="${nextEl.preview}"
+    data-source="${nextEl.original}"
+    alt="${nextEl.description}"
+  />
+</a>
+</div>`, ``);
+
+galleryEl.innerHTML = createGgalleryItem;
+
+galleryEl.addEventListener('click', openModalGallery);
+
+function openModalGallery(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') return;
+
+    const modalGallery = basicLightbox.create(`<img src="${event.target.dataset.source}" width="800" height="600">`);
+    modalGallery.show();
+
+    galleryEl.addEventListener('keydown', (event) => {
+        if (event.code === 'Escape') {
+            modalGallery.close()
+        }
+    }, {once: true })
+};
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery')
+
+const createGalleryItem = galleryItems.reduce((html, nextItem) => html + 
+`<a class="gallery__item" href="${nextItem.original}">
+<img class="gallery__image" src="${nextItem.preview}" alt="${nextItem.description}" />
+</a>`, ``);
+
+galleryEl.innerHTML = createGalleryItem;
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    docClose: true,
+    enableKeyboard: true,
+    loop: true,
+
+    captions: true, 
+    captionSelector: '.gallery__image',
+    captionType: 'attr',
+    captionsData: 'alt', 
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
+       
+
+        
